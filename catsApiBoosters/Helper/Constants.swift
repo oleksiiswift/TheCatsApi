dc97064ecf75c8f5f4a36682101c742c9f2bbd7e
alekseiisorochan <alekseiisorochan@gmail.com>
2022-06-20 13:10:29 +0300

    CA-01 add objects model, create realm persistant manager

---
+++ catsApiBoosters/Helper/Constants.swift
@@ -0,0 +1,37 @@
+//
+//  Constants.swift
+//  catsApiBoosters
+//
+//  Created by alexey sorochan on 20.06.2022.
+//
+
+import Foundation
+
+struct Constants {
+	
+	
+	struct Identifiers {
+		
+		struct Stroryboards {
+			
+		}
+		
+		struct ViewControllers {
+			
+		}
+		
+		struct Cells {
+			
+		}
+		
+		struct Xibs {
+			
+		}
+	}
+	
+	struct CodingKeysRawValues {
+		
+		static let free = "free"
+		static let paid = "paid"
+	}
+}
---
+++ catsApiBoosters/Helper/ErrorHandler.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorHandler.swift
+//  catsApiBoosters
+//
+//  Created by alexey sorochan on 20.06.2022.
+//
+
+import Foundation
